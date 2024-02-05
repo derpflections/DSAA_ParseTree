dad23b3ba77d05289a89c13f32b708b164e98b1b
derpflections <leehongyi91@gmail.com>
2024-02-05 23:01:19 +0800

    im love new dependency

--- application/menu.py
+++ application/menu.py
@@ -16,6 +16,8 @@ from application.Variable import Variable
 from application.FileReader import fileManipulator
 from application.AssignmentEvaluator import AssignmentEvaluator
 from application.ParseInsert import ParseInserter
+from application.DependencyIdentifier import DependencyIdentifier
+
 class MainMenu:
     def __init__(self, options = None):
         self.border_length = 71
@@ -137,6 +139,31 @@ class MainMenu:
             opt5_fileManipulator.write_contents(opt5_evaluator.evaluation().split('\n'))
             # print(self.Hash)
     
+<<<<<<< Updated upstream
 
+=======
+        elif int(selection) == 6:
+            pass
+        elif int(selection) == 7:
+            final_output_str = ""
+            opt7_dependency = DependencyIdentifier(self.Hash)
+            dependency_list = opt7_dependency.parse_assignments()
+            for var_name in dependency_list:
+                internal_str = ""
+                if len(dependency_list[var_name]) != 0:
+                    internal_str += f"{var_name} depends on:"
+                    for dependency in dependency_list[var_name]:
+                        internal_str += f" {dependency},"
+                    internal_str = internal_str[:-1] + "."
+                else:
+                    internal_str += f"{var_name} has no dependencies."
+                final_output_str += f"{internal_str} \n"
+            print("\n\nDependencies:\n*************")
+            print(f"{final_output_str}")
+        elif int(selection) == 8:
+            pass
+        elif int(selection) == 9:
+            pass
+>>>>>>> Stashed changes
 
-##726132
+##7261232
